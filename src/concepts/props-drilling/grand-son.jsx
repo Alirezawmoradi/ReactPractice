9583fd13405324bd09efd97ae41f952f6c62bcb4
Alirezaw <Alirezawmoradi@gmail.com>
2024-02-10 23:48:29 +0330

    Prop drilling problem fixed with app context concept

--- src/concepts/props-drilling/grand-son.jsx
+++ src/concepts/props-drilling/grand-son.jsx
@@ -2,7 +2,6 @@ import {useContext} from "react";
 import {AppContext} from "../app-context/app-context.jsx";
 
 const GrandSon = () => {
-
     const {state} = useContext(AppContext);
     return (
         <h1>{state}</h1>
