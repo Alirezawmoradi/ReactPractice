08aa450620f35e6ebe09c830f803bbe45ba6dddb
Alirezaw <Alirezawmoradi@gmail.com>
2024-02-19 23:21:52 +0330

    batch state update concept completed

--- src/concepts/batch-state-upadte/batch-state-update.jsx
+++ src/concepts/batch-state-upadte/batch-state-update.jsx
@@ -1,5 +1,22 @@
+import {useEffect, useState} from "react";
+
 export const BatchStateUpdate = () => {
+    console.log('render');
+    const [name, setName] = useState();
+    const updateState = () => {
+        setName('Ali');
+        setName('Hamid');
+        setName('Kamyab');
+        setName('Reza');
+    }
+
+    useEffect(() => {
+        setName('Alireza')
+    }, [])
     return (
-        <>Batch State Update Concept</>
+        <>
+            <span>{name}</span>
+            <button onClick={updateState}>update state</button>
+        </>
     )
 }
